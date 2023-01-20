732447b0b8d8083be473f4a448ebc82a45ead81e
Chris James <quii@hey.com>
2023-01-20 22:28:12 +0000

    stuff

--- test_iteration.py
+++ test_iteration.py
@@ -27,6 +27,24 @@ class MyTestCase(unittest.TestCase):
         squares = [value**2 for value in range(1, 11)]
         self.assertEqual([1, 4, 9, 16, 25, 36, 49, 64, 81, 100], squares)
 
+        # slices just like go, and you can use them in for too
+        self .assertEqual([4, 9, 16], squares[1:4])
+        squares_copy = squares[:]
+        squares_copy.pop()
+        self.assertEqual([1, 4, 9, 16, 25, 36, 49, 64, 81, 100], squares)
+        self.assertEqual([1, 4, 9, 16, 25, 36, 49, 64, 81], squares_copy)
+
+    def test_tuples(self):
+        # tuples are immutable lists
+        dimensions = (200, 50)
+        self.assertEqual(200, dimensions[0])
+        # dimensions[0] = 250 <- cant do this
+
+        # but you can override the variable
+        dimensions = (100, 50)
+        self.assertEqual(100, dimensions[0])
+
+
 
 if __name__ == '__main__':
     unittest.main()
