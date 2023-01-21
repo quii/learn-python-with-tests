24ce75bfd8493fa6940ff724d2e812a32299d4c1
Chris James <quii@hey.com>
2023-01-21 08:49:14 +0000

    reformat

--- test_hello.py
+++ test_hello.py
@@ -2,9 +2,11 @@ import pytest
 
 from hello import Greeter
 
+
 @pytest.fixture
 def greeter():
     return Greeter("Hello")
+
 
 def test_greet(greeter):
     assert greeter.greet("world") == "Hello, world"
