26a8ecb94679d50944a098849da57dccf78fb6f5
Chris James <quii@hey.com>
2023-01-21 08:47:11 +0000

    use a fixture

--- test_lists.py
+++ test_lists.py
@@ -1,40 +1,66 @@
-def test_lists():
-    people = ['chris', 'ruth', 'pepper']
-    assert people[0].title() == "Chris"
-    assert people[-1] == "pepper"
+import pytest
 
-    # todo: this is begging to be a test fixture, learn how to do tha
+
+@pytest.fixture
+def people():
+    return ['chris', 'ruth', 'pepper']
+
+
+def test_list_index_ops(people):
+    assert people[0].title() == "Chris"
+    assert people[-1] == "pepper", "last person should be pepper"
+
+
+def test_list_append(people):
     people.append('tiago')
     assert people[-1] == 'tiago'
+
+
+def test_list_delete(people):
     del people[-1]
-    assert people[-1] == 'pepper'
-
-    people.insert(0, 'dave')
-    assert people[0]=='dave'
-
-    last_person = people.pop()
-    assert last_person == 'pepper'
     assert people[-1] == 'ruth'
 
-    colours = ['red', 'yellow', 'blue']
-    yellow = colours.pop(1)
-    assert yellow == 'yellow'
-    assert colours == ['red', 'blue']
-    colours.remove('red')
-    assert colours == ['blue']
 
-    # remove only removes one thing
+def test_insert_at_index(people):
+    people.insert(0, 'dave')
+    assert people[0] == 'dave'
+
+
+def test_pop(people):
+    person = people.pop()
+    assert person == 'pepper'
+    assert people == ['chris', 'ruth']
+
+
+def test_pop_at_index(people):
+    person = people.pop(1)
+    assert person == 'ruth'
+    assert people == ['chris', 'pepper']
+
+
+def test_remove(people):
+    people.remove('chris')
+    assert people == ['ruth', 'pepper']
+
+
+def test_sort(people):
+    people.sort()
+    assert people == ['chris', 'pepper', 'ruth']
+    people.sort(reverse=True)
+    assert people == ['ruth', 'pepper', 'chris']
+
+
+def test_sorted(people):
+    sorted_people = sorted(people)
+    assert people == ['chris', 'ruth', 'pepper']
+    assert sorted_people == ['chris', 'pepper', 'ruth']
+
+
+def test_len(people):
+    assert len(people) == 3
+
+
+def test_remove_multiple():
     numbers = [1, 1, 1]
     numbers.remove(1)
     assert numbers == [1, 1]
-
-    numbers = [2, 1, 3]
-    numbers.sort()
-    assert numbers == [1, 2, 3]
-
-    numbers.sort(reverse=True)
-    assert numbers == [3, 2, 1]
-    other_numbers = sorted(numbers)
-    assert numbers == [3, 2, 1]
-    assert other_numbers == [1, 2, 3]
-    assert len(numbers) == 3
