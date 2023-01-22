3808df29647fc3c83923ebc759539072986c117c
Chris James <quii@hey.com>
2023-01-22 08:48:47 +0000

    parametrize tests

--- test_adding.py
+++ test_adding.py
@@ -1,6 +1,10 @@
+import pytest
+
 from adding import add
 
-
-def test_add():
-    assert add(2, 2) == 4
-    assert add(1_000, 2_000) == 3_000
+@pytest.mark.parametrize("x,y,expected", [
+    (2, 2, 4),
+    (1_000, 2_000, 3_000),
+])
+def test_add(x, y, expected):
+    assert add(x, y) == expected
